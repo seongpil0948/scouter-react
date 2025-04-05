c3281eec78bacd9629cc79e213f97d65a2e4cdc7
Seongpil Choi <seongpil0948@gmail.com>
2025-04-06 03:06:31 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { siteConfig } from '@/config/site';
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">hi</div>
+    </section>
+  );
+}
